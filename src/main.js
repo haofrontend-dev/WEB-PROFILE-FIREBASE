efeba891020a722e674bc52274056eeede56aaa3
haofrontend-dev <haoqn.work@gmail.com>
2023-02-05 01:11:14 +0700

    Commit Code 2

--- src/main.js
+++ src/main.js
@@ -2,17 +2,20 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
+import Slick from "vue-slick";
+import "slick-carousel/slick/slick.css";
+
+import "@/assets/css/global.css";
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
 Vue.config.productionTip = false;
-
+Vue.component("slick-slider", Slick);
 new Vue({
   router,
   render: (h) => h(App),
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,50 @@
 <template>
   <div class="home">
-    <div class="banner" :style="{ backgroundImage: `url(${banner})` }"></div>
-    <div class="container">
-      <div class="row">
-        <div class="col-5 image-about">
-          <img src="@/assets/image/image-about.png" alt="" />
+    <div class="banner">
+      <img src="@/assets/image/banner.png" alt="" />
+    </div>
+    <div class="content" :style="{ backgroundImage: `url(${bgSkill})` }">
+      <div class="bg-white">
+        <div class="container pt-5">
+          <div class="row">
+            <div class="col-5 image-about">
+              <img src="@/assets/image/image-about.png" alt="" />
+            </div>
+            <div class="col-7 about-me">
+              <div>
+                <h2 class="text-header">About me</h2>
+                <p class="pe-5">
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+                  diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+                  aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
+                  nostrud exerci tation ullamcorper suscipit lobortis nisl ut
+                  aliquip ex ea commodo consequat. Duis
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="col-7 about-me">
-          <div>
-            <h2>About me</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-              nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
-              erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
-              tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
-              consequat. Duis
-            </p>
+      </div>
+      <div class="section-skill">
+        <div class="container">
+          <div class="section-skill">
+            <h2 class="text-white fw-bold text-center mt-3">Graphic designs</h2>
+            <div class="mt-5">
+              <slick-slider :options="optionsGP">
+                <div>
+                  <img src="@/assets/image/gp-1.png" alt="" width="100%" />
+                </div>
+                <div>
+                  <img src="@/assets/image/gp-2.png" alt="" width="100%" />
+                </div>
+                <div>
+                  <img src="@/assets/image/gp-3.png" alt="" width="100%" />
+                </div>
+                <div>
+                  <img src="@/assets/image/gp-3.png" alt="" width="100%" />
+                </div>
+              </slick-slider>
+            </div>
           </div>
         </div>
       </div>
@@ -24,30 +53,72 @@
 </template>
 
 <script>
-import banner from "@/assets/image/banner.png";
-
+import bgSkill from "@/assets/image/bg-skill.png";
 export default {
   name: "HomeView",
   data() {
     return {
       imageBanner: "'image/banner.png'",
-      banner,
+      bgSkill,
+      optionsGP: {
+        centerMode: true,
+        centerPadding: "",
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        infinite: true,
+        prevArrow:
+          "<button type='button' class='slick-prev pull-left'></button>",
+        nextArrow:
+          "<button type='button' class='slick-next pull-right'></button>",
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: "40px",
+              slidesToShow: 3,
+            },
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: "40px",
+              slidesToShow: 1,
+            },
+          },
+        ],
+      },
     };
   },
 };
 </script>
 <style scoped>
-.banner {
-  height: 760px;
-  padding: 90px 0;
+.banner img {
+  width: 100%;
+}
+.content {
   background-repeat: no-repeat;
-  background-size: contain;
+  background-size: cover;
 }
 .image-about img {
   width: 360px;
-  margin-top: -40px;
 }
 .about-me p {
   text-align: justify;
+}
+.about-me .text-header {
+  color: #6b9cff;
+}
+.about-me p {
+  color: #033333;
+}
+
+.cl {
+  height: 200px;
+  padding: 10px;
 }
 </style>
