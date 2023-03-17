106c969cc245504434301cab0602bbde2446458e
haofrontend-dev <haoqn.work@gmail.com>
2023-03-17 14:19:25 +0700

    Push code

--- src/config/filsebase.js
+++ src/config/filsebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyAPG5KbnxLr5Jh3kEWb5EzIvjJvcjO_IcY",
-  authDomain: "test-b99d9.firebaseapp.com",
-  projectId: "test-b99d9",
-  storageBucket: "test-b99d9.appspot.com",
-  messagingSenderId: "556640825709",
-  appId: "1:556640825709:web:631c7e6421a539a386ea64",
-  measurementId: "G-RLC1PXH83C",
+  apiKey: "AIzaSyCnFVwUQ3EdQeF8ufNxDSnTAP_ZIEiknAY",
+  authDomain: "river-runner-379116.firebaseapp.com",
+  projectId: "river-runner-379116",
+  storageBucket: "river-runner-379116.appspot.com",
+  messagingSenderId: "104945032599",
+  appId: "1:104945032599:web:a1dff6be01c3e189e9f8d2",
+  measurementId: "G-32GTNSQ9PW",
 };
 const appData = initializeApp(firebaseConfig);
 const auth = getAuth(appData);
